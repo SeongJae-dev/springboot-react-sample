bd52c744b587f5eb58a5ec721062d94436e1bc33
Jo <chiron1217@naver.com>
2021-03-12 23:55:50 +0900

    init

---
+++ src/main/java/com/example/springbootreactsample/SpringbootReactSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootreactsample/SpringbootReactSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
